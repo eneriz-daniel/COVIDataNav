408913a76c1adcd30196be1915799100daedddce
Daniel Enériz Orta <57150162+eneriz-daniel@users.noreply.github.com>
2020-10-15 10:55:16 +0200

    Versión 1.1
    Añadida NAVARRA para generar resultados

--- data_updater.py
+++ data_updater.py
@@ -5,9 +5,11 @@ casos positivos en el último día, en los últimos 15 días y los acumulados de
 el incio de la pandemia para esa localidad. Además se genera una gráfica con
 la evolución de la pandemia en esa localidad.
 
+Además hace lo mismo para toda Navarra
+
 Se descartan todos los casos nulos (sin información de localdiad) y los de Fuera de Navarra.
 
-Version 1.0
+Version 1.1
 Daniel Enériz Orta
 """
 
@@ -20,44 +22,13 @@ import json
 import os
 import locale
 
-locale.setlocale(locale.LC_TIME, 'es_ES')
+def GeneraDatos(dfmun_tot, municipio):
+        #Calculammos los confirmados en los últimos 15 días, los casos en el último día y los acumulados hasta hoy
+    casos_15dias = int(dfmun_tot.last('15D').NuevosCasos.sum())
 
-df = pd.read_csv('CasosMunicipios_ZR_Covid.csv',encoding='latin-1', delimiter=';', parse_dates=[0]) # Cargamos datos
+    casos_ultimodia = int(dfmun_tot.last('1D').NuevosCasos.sum())
 
-df = df[df.CodZR != -1] #Eliminamos los datos Nulos/Sin informar
-df = df[df.CodMun != 0] #Eliminamos los datos de positivos de otras Comunidades Autónomas
-df = df[df.CodZR != 99]
-
-df = df.replace([' / ', '-'], '_', regex=True) # Renombremos los nombres en formato CASTELLANO / EUSKERA y CASTELLANO-EUSKERA para poder usarlos para crear directorios
-
-                                       # Creamos un directorio donde guardar los datos resumidos en caso de que no exista
-for municipio in df.DesMun.unique():
-    try:
-        os.mkdir('./Datos_municipios/{}'.format(municipio))
-    except(FileExistsError):
-        pass
-
-df.Fecha = pd.to_datetime(df.Fecha) # Asignamos formato fecha a la fecha
-df = df.set_index('Fecha')          # y lo usamos como ínidce para usar el atributo .last
-
-for municipio in df.DesMun.unique():   # Recorremos todos los municipios para recopilar los datos de cada uno
-    dfmun = df[df.DesMun == municipio]
-
-    dfmun_tot = pd.DataFrame(0, index=pd.date_range(start=df.first('D').index[0]-datetime.timedelta(days=1), end=df.last('D').index[0], freq='D'), columns=['NuevosCasos', 'AcumuladoCasosHastaLaFecha'])
-
-    for fecha in dfmun_tot.index.array:
-        if fecha in dfmun.index.array:
-            dfmun_tot.loc[fecha].NuevosCasos +=  dfmun.loc[fecha].NuevosCasos.sum()
-            dfmun_tot.loc[fecha].AcumuladoCasosHastaLaFecha =  dfmun_tot.loc[fecha-datetime.timedelta(days=1)].AcumuladoCasosHastaLaFecha + dfmun.loc[fecha].NuevosCasos.sum()
-        elif fecha != df.first('D').index[0]-datetime.timedelta(days=1):
-            dfmun_tot.loc[fecha].AcumuladoCasosHastaLaFecha = dfmun_tot.loc[fecha-datetime.timedelta(days=1)].AcumuladoCasosHastaLaFecha
-
-    #Calculammos los confirmados en los últimos 15 días, el relativo de estos cada 100000 habs, los casos en el último día y los acumulados hasta hoy
-    casos_15dias = int(dfmun.last('1D').AcumuladoCasosHastaLaFecha.array.sum() - dfmun.last('15D')[dfmun.last('15D').index==dfmun.last('15D').index.array.min()].AcumuladoCasosHastaLaFecha.sum()  + dfmun.last('15D')[dfmun.last('15D').index==dfmun.last('15D').index.array.min()].NuevosCasos.sum())
-
-    casos_ultimodia = int(dfmun.last('1D').NuevosCasos.array.sum())
-
-    acumulados_hasta_hoy = int(dfmun.last('1D').AcumuladoCasosHastaLaFecha.array.sum())
+    acumulados_hasta_hoy = int(dfmun_tot.last('1D').AcumuladoCasosHastaLaFecha.sum())
 
     datos_municipio = {'Municipio': municipio, 'Fecha': df.last('1D').index[0].strftime('%d de %B de %Y'), 'Datos':{'Casos15dias': casos_15dias, 'CasosUltimoDia': casos_ultimodia, 'CasosAcum': acumulados_hasta_hoy}}
 
@@ -65,7 +36,6 @@ for municipio in df.DesMun.unique():   # Recorremos todos los municipios para re
         json.dump(datos_municipio, json_fout)
 
     print('{}:\n\tÚltimo día: {}\n\t15 días: {}\n\tAcumulados: {}'.format(municipio, casos_ultimodia, casos_15dias, acumulados_hasta_hoy))
-
 
     fig, ax2 = plt.subplots()
 
@@ -94,3 +64,63 @@ for municipio in df.DesMun.unique():   # Recorremos todos los municipios para re
     #plt.show()
 
     plt.close(fig)
+
+locale.setlocale(locale.LC_TIME, 'es_ES')
+
+df = pd.read_csv('CasosMunicipios_ZR_Covid.csv',encoding='latin-1', delimiter=';', parse_dates=[0]) # Cargamos datos
+
+df = df[df.CodZR != -1] #Eliminamos los datos Nulos/Sin informar
+df = df[df.CodMun != 0] #Eliminamos los datos de positivos de otras Comunidades Autónomas
+df = df[df.CodZR != 99]
+
+df = df.replace([' / ', '-'], '_', regex=True) # Renombremos los nombres en formato CASTELLANO / EUSKERA y CASTELLANO-EUSKERA para poder usarlos para crear directorios
+
+                                       # Creamos un directorio donde guardar los datos resumidos en caso de que no exista
+for municipio in df.DesMun.unique():
+    try:
+        os.mkdir('./Datos_municipios/{}'.format(municipio))
+    except(FileExistsError):
+        pass
+try:
+    os.mkdir('./Datos_municipios/NAVARRA')
+except(FileExistsError):
+    pass
+
+df.Fecha = pd.to_datetime(df.Fecha) # Asignamos formato fecha a la fecha
+df = df.set_index('Fecha')          # y lo usamos como ínidce para usar el atributo .last
+
+# Calculamos para todo Navarra
+dfmun = df
+
+municipio = 'NAVARRA'
+
+dfmun_tot = pd.DataFrame(0, index=pd.date_range(start=df.first('D').index[0]-datetime.timedelta(days=1), end=df.last('D').index[0], freq='D'), columns=['NuevosCasos', 'AcumuladoCasosHastaLaFecha'])
+
+for fecha in dfmun_tot.index:
+    if fecha in dfmun.index:
+        dfmun_tot.loc[fecha].NuevosCasos +=  dfmun.loc[fecha].NuevosCasos.sum()
+        dfmun_tot.loc[fecha].AcumuladoCasosHastaLaFecha =  dfmun_tot.loc[fecha-datetime.timedelta(days=1)].AcumuladoCasosHastaLaFecha + dfmun.loc[fecha].NuevosCasos.sum()
+    elif fecha != df.first('D').index[0]-datetime.timedelta(days=1):
+        dfmun_tot.loc[fecha].AcumuladoCasosHastaLaFecha = dfmun_tot.loc[fecha-datetime.timedelta(days=1)].AcumuladoCasosHastaLaFecha
+
+GeneraDatos(dfmun_tot, municipio)
+
+
+contador = 1
+
+for municipio in df.DesMun.unique():   # Recorremos todos los municipios para recopilar los datos de cada uno
+    dfmun = df[df.DesMun == municipio]
+
+    dfmun_tot = pd.DataFrame(0, index=pd.date_range(start=df.first('D').index[0]-datetime.timedelta(days=1), end=df.last('D').index[0], freq='D'), columns=['NuevosCasos', 'AcumuladoCasosHastaLaFecha'])
+
+    for fecha in dfmun_tot.index:
+        if fecha in dfmun.index:
+            dfmun_tot.loc[fecha].NuevosCasos +=  dfmun.loc[fecha].NuevosCasos.sum()
+            dfmun_tot.loc[fecha].AcumuladoCasosHastaLaFecha =  dfmun_tot.loc[fecha-datetime.timedelta(days=1)].AcumuladoCasosHastaLaFecha + dfmun.loc[fecha].NuevosCasos.sum()
+        elif fecha != df.first('D').index[0]-datetime.timedelta(days=1):
+            dfmun_tot.loc[fecha].AcumuladoCasosHastaLaFecha = dfmun_tot.loc[fecha-datetime.timedelta(days=1)].AcumuladoCasosHastaLaFecha
+
+    GeneraDatos(dfmun_tot, municipio)
+
+    contador += 1
+    print('\n\n{}/{}: '.format(contador, len(df.DesMun.unique())))
